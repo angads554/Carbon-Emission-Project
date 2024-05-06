500401db714af504d7a64be8d4aad7cf0f4bd233
angads554 <angads554@gmail.com>
2024-05-06 10:15:24 -0500

    Final commit

---
+++ CarbonEmission.R
@@ -0,0 +1,30 @@
+FLAGS<- flags(
+  flag_numeric("nodes", 32),
+  flag_numeric("batch_size",32),
+  flag_string("activation","relu"),
+  flag_numeric("learning_rate",0.01),
+  flag_numeric("dropout", 0.2)
+)
+
+model = keras_model_sequential()
+
+model %>%
+  layer_dense(units = FLAGS$nodes, activation = FLAGS$activation, input_shape = dim(carbonTrainingFinal)[2]) %>%
+  layer_dropout(rate=FLAGS$dropout)%>%
+  layer_dense(units = FLAGS$nodes, activation = FLAGS$activation) %>%
+  layer_dropout(rate=FLAGS$dropout)%>%
+  layer_dense(units = FLAGS$nodes, activation = FLAGS$activation) %>%
+  layer_dropout(rate=FLAGS$dropout)%>%
+  layer_dense(units = 1)
+
+model %>% compile(
+  loss="mse",
+  optimizer=optimizer_adam(lr=FLAGS$learning_rate)
+)
+
+model %>%fit(  as.matrix(carbonTrainingFinal),
+               carbonTrainingLabels,
+               batch_size=FLAGS$batch_size,
+               epochs=20,
+               validation_data = list(as.matrix(carbonValidationFinal),carbonValidationLabels),verbose = 2
+)
